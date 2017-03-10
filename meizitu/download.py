68c932bc5f3791589f322b99f9df6c5e775bbc47
yangbo <yangbofox@foxmail.com>
2017-03-10 04:26:28 -0500

    update and modifiy

---
+++ meizitu/download.py
@@ -0,0 +1,80 @@
+# coding=utf-8
+
+import re
+import random
+import time
+import requests
+from requests.exceptions import ConnectionError
+
+
+class download(object):
+
+    def __init__(self):
+        self.user_agent_list = [
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
+            "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
+            "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
+            "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",
+            "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+            "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+            "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+            "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+            "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+            "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+            "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"]
+
+        self.iplist = []
+        html = requests.get("http://haoip.cc/tiqu.htm")
+        iplisten = re.findall(r'r/>(.*?)<b', html.text, re.S)
+        for ip in iplisten:
+            i = re.sub('\n', '', ip)
+            self.iplist.append(i.strip())
+
+    def get(self, url, timeout, proxy=None, num_retries=6):
+        UA = random.choice(self.user_agent_list)
+        headers = {'User-Agent': UA}
+
+        if proxy is None:
+            try:
+                return requests.get(url, headers=headers, timeout=timeout)
+            except:
+                if num_retries > 0:
+                    time.sleep(10)
+                    print(u'获取网页出错，10S后将获取倒数第：', num_retries, u'次')
+                    num_retries -= 1
+                    return self.get(url, timeout, num_retries)
+                else:
+                    print(u'开始使用代理')
+                    time.sleep(10)
+                    IP = ''.join(str(random.choice(self.iplist)).strip())
+                    proxy = {'http': IP}
+                    return self.get(url, timeout, proxy)
+        else:
+            try:
+                IP = ''.join(str(random.choice(self.iplist)).strip())
+                print(IP)
+                proxy = {'http': IP}
+                print(proxy)
+                return requests.get(
+                    url, headers=headers, proxy=proxy, timeout=timeout)
+            except:
+                if num_retries > 0:
+                    time.sleep(10)
+
+                    IP = ''.join(str(random.choice(self.iplist)).strip())
+                    proxy = {'http': IP}
+
+                    print(u'正在更换代理，10S后将重新获取倒数第', num_retries, u'次')
+                    print(u'当前代理是：', proxy)
+                    return self.get(url, timeout, proxy, num_retries - 1)
+
+                else:
+                    print(u'代理不能使用！取消代理')
+                    return self.get(url, 3)
--- meizitu/spider_meizitu.py
+++ meizitu/spider_meizitu.py
@@ -4,35 +4,38 @@ import os
 import requests
 from bs4 import BeautifulSoup
 from hashlib import md5
+from download import download
 
 headers = {
     'User-Agent': "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1"}
 
-class meizitu:
+
+class meizitu(download):
 
     def __init__(self, url):
+        super(meizitu, self).__init__()
         self.url = url
 
-    def request(self, url):
-        content = requests.get(url, headers=headers)
-        return content
-
     def html(self, href):
-        html = self.request(href)
-        max_span = BeautifulSoup(html.text, 'lxml').find('div', class_='pagenavi').find_all('span')[-2].get_text()
+        html = download.get(self, href, 3)
+        max_span = BeautifulSoup(html.text, 'lxml').find(
+            'div', class_='pagenavi').find_all('span')[-2].get_text()
 
-        for page in range(1, int(max_span)+1):
+        for page in range(1, int(max_span) + 1):
             page_url = href + '/' + str(page)
             self.img(page_url)
 
     def img(self, page_url):
-        img_html = self.request(page_url)
-        img_url = BeautifulSoup(img_html.text, 'lxml').find('div', class_='main-image').find('img')['src']
+        img_html = download.get(self, page_url, 3)
+        img_url = BeautifulSoup(
+            img_html.text, 'lxml').find(
+            'div', class_='main-image').find('img')['src']
         self.save(img_url)
 
     def save(self, img_url):
         name = md5(str(img_url).encode(encoding='utf-8')).hexdigest()
-        img = self.request(img_url)
+        img = download.get(self, img_url, 3)
+        print('正在下载：{0}\n'.format(img_url))
         with open(str(name) + '.jpg', 'ab')as f:
             f.write(img.content)
 
@@ -49,8 +52,10 @@ class meizitu:
             return False
 
     def all_url(self):
-        html = self.request(self.url)
-        all_a = BeautifulSoup(html.text, 'lxml').find('div', class_='all').find_all('a')
+        html = download.get(self, self.url, 3)
+        all_a = BeautifulSoup(
+            html.text, 'lxml').find(
+            'div', class_='all').find_all('a')
         for a in all_a:
             title = a.get_text()
             print(u'开始保存：', title)
@@ -58,6 +63,7 @@ class meizitu:
             self.mkdir(path)
             href = a['href']
             self.html(href)
+
 
 if __name__ == '__main__':
     url = 'http://www.mzitu.com/all'
