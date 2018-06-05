71585c442c066f32e93dd7f348caa239f833e08b
yangbo <yangbo@testbird.com>
2018-06-05 19:42:21 +0800

    fix find sms bug

--- gpprivacy/gp_privacy_crawler.py
+++ gpprivacy/gp_privacy_crawler.py
@@ -92,8 +92,8 @@ def get_privacy(driver, package_name):
         tmp = (By.CLASS_NAME, "fnLizd")
         WebDriverWait(driver, 20).until(EC.presence_of_element_located(tmp))
         page_source = driver.find_element_by_class_name("fnLizd").get_attribute("innerHTML")
-        # print(page_source)
-        if "短信" or "SMS" in page_source:
+        print(page_source)
+        if "SMS" in page_source or "短信" in page_source:
             print("找到含有SMS权限的APP: {0}".format(package_name))
             with open("privacy_with_sms.txt", "a+") as f:
                 f.write(package_name + "\n")
@@ -124,7 +124,8 @@ def main():
 if __name__ == "__main__":
     print("start")
     # driver = _init_chrome_driver(0)
-    # pack = get_privacy(driver, "com.verizon.messaging.vzmsgs")
+    # pack = get_privacy(driver, "com.halfbrick.fruitninjafree")
+    # driver.quit()
     # pack = get_privacy(driver, "com.magnet.torrent.cat")
     main()
     print("done")
